b389c15e41f8aa6697ffab9b436d54b9eac39545
barnesgr <barnesgr@googlemail.com>
2013-02-15 12:15:14 +0000

    minor updates

--- private/get_data_features.m
+++ private/get_data_features.m
@@ -2,11 +2,12 @@
 function [trialfeatures,vedata]=get_data_features(flatpepdata,Nbins,Ntrials,weights,Tfull,datatype,featureind,regressout)
 
 %%% put data in form for mv test
-%% options are datatype=imre: evoked (real + complex per freq band)
+%% options are datatye
+%% {'peakabs','peakreal','peakimag','peaksin','peakcos','sumpower'};
 %% datatype=pwr power (re^2+comp^2)
 %% datatpe=pwrsincos: power+sin and cos terms
 %% datatype=4:
-
+%% if weights==-1 just set up static variables depnding on datatype
 %% 
 flagnames={'peakabs','peakreal','peakimag','peaksin','peakcos','sumpower'};
 
