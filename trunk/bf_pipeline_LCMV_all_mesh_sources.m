9e09bcc9c8dfc6e07b0da688bf37fcf5dbe01358
litvak.vladimir@gmail.com <litvak.vladimir@gmail.com@2517a067-387d-feba-a968-2df69eff7c46>
2014-10-31 15:41:08 +0000

    new pipeline

--- trunk/bf_pipeline_LCMV_all_mesh_sources.m
+++ trunk/bf_pipeline_LCMV_all_mesh_sources.m
@@ -1,5 +1,5 @@
 %-----------------------------------------------------------------------
-% Job saved on 31-Oct-2014 14:46:20 by cfg_util (rev $Rev$)
+% Job saved on 31-Oct-2014 13:48:12 by cfg_util (rev $Rev$)
 % spm SPM - SPM12 (12.0)
 % cfg_basicio BasicIO - Unknown
 %-----------------------------------------------------------------------
@@ -16,7 +16,7 @@ matlabbatch{1}.spm.meeg.source.headmodel.coregistration.coregspecify.fiducial(3)
 matlabbatch{1}.spm.meeg.source.headmodel.coregistration.coregspecify.useheadshape = 0;
 matlabbatch{1}.spm.meeg.source.headmodel.forward.eeg = 'EEG BEM';
 matlabbatch{1}.spm.meeg.source.headmodel.forward.meg = 'Single Shell';
-matlabbatch{2}.spm.tools.beamforming.data.D(1) = cfg_dep('Head model specification: M/EEG dataset(s) with a forward model', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','D'));
+matlabbatch{2}.spm.tools.beamforming.data.D(1) = cfg_dep('M/EEG head model specification: M/EEG dataset(s) with a forward model', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','D'));
 matlabbatch{2}.spm.tools.beamforming.data.val = 1;
 matlabbatch{2}.spm.tools.beamforming.data.gradsource = 'inv';
 matlabbatch{2}.spm.tools.beamforming.data.space = 'MNI-aligned';
@@ -38,30 +38,13 @@ matlabbatch{4}.spm.tools.beamforming.features.plugin.cov.taper = 'none';
 matlabbatch{4}.spm.tools.beamforming.features.regularisation.manual.lambda = 1;
 matlabbatch{4}.spm.tools.beamforming.features.bootstrap = false;
 matlabbatch{5}.spm.tools.beamforming.inverse.BF(1) = cfg_dep('Covariance features: BF.mat file', substruct('.','val', '{}',{4}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','BF'));
-matlabbatch{5}.spm.tools.beamforming.inverse.plugin.deflect.filter.label = 'lA1';
-matlabbatch{5}.spm.tools.beamforming.inverse.plugin.deflect.filter.passband{1}.voi.pos = [-58 -19 -2];
-matlabbatch{5}.spm.tools.beamforming.inverse.plugin.deflect.filter.passband{1}.voi.radius = 5;
-matlabbatch{5}.spm.tools.beamforming.inverse.plugin.deflect.filter.svdpassband = 0;
-matlabbatch{5}.spm.tools.beamforming.inverse.plugin.deflect.filter.forcepassband = 1;
-matlabbatch{5}.spm.tools.beamforming.inverse.plugin.deflect.filter.stopband = cell(1, 0);
-matlabbatch{5}.spm.tools.beamforming.inverse.plugin.deflect.filter.svdstopband = 0;
-matlabbatch{5}.spm.tools.beamforming.inverse.plugin.deflect.filter.usecov = 1;
-matlabbatch{5}.spm.tools.beamforming.inverse.plugin.deflect.snr = 5;
-matlabbatch{5}.spm.tools.beamforming.inverse.plugin.deflect.trunc = 0;
+matlabbatch{5}.spm.tools.beamforming.inverse.plugin.lcmv.orient = true;
+matlabbatch{5}.spm.tools.beamforming.inverse.plugin.lcmv.keeplf = false;
 matlabbatch{6}.spm.tools.beamforming.output.BF(1) = cfg_dep('Inverse solution: BF.mat file', substruct('.','val', '{}',{5}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','BF'));
-matlabbatch{6}.spm.tools.beamforming.output.plugin.montage.method = 'max';
+matlabbatch{6}.spm.tools.beamforming.output.plugin.montage.method = 'keep';
 matlabbatch{6}.spm.tools.beamforming.output.plugin.montage.voidef = struct('label', {}, 'pos', {}, 'radius', {});
 matlabbatch{7}.spm.tools.beamforming.write.BF(1) = cfg_dep('Output: BF.mat file', substruct('.','val', '{}',{6}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','BF'));
-matlabbatch{7}.spm.tools.beamforming.write.plugin.spmeeg.mode = 'write';
+matlabbatch{7}.spm.tools.beamforming.write.plugin.spmeeg.mode = 'onlinecopy';
 matlabbatch{7}.spm.tools.beamforming.write.plugin.spmeeg.modality = 'MEG';
 matlabbatch{7}.spm.tools.beamforming.write.plugin.spmeeg.addchannels.none = 0;
 matlabbatch{7}.spm.tools.beamforming.write.plugin.spmeeg.prefix = 'B';
-matlabbatch{8}.spm.meeg.other.review.D(1) = cfg_dep('Write: Output files', substruct('.','val', '{}',{7}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','files'));
-matlabbatch{9}.spm.tools.beamforming.output.BF(1) = cfg_dep('Inverse solution: BF.mat file', substruct('.','val', '{}',{5}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','BF'));
-matlabbatch{9}.spm.tools.beamforming.output.plugin.image_filtcorr.seedspec.label = 'lA1';
-matlabbatch{9}.spm.tools.beamforming.output.plugin.image_filtcorr.corrtype = 'filtlf';
-matlabbatch{9}.spm.tools.beamforming.output.plugin.image_filtcorr.modality = 'MEG';
-matlabbatch{10}.spm.tools.beamforming.write.BF(1) = cfg_dep('Output: BF.mat file', substruct('.','val', '{}',{9}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','BF'));
-matlabbatch{10}.spm.tools.beamforming.write.plugin.gifti.normalise = 'no';
-matlabbatch{10}.spm.tools.beamforming.write.plugin.gifti.space = 'mni';
-matlabbatch{10}.spm.tools.beamforming.write.plugin.gifti.visualise = 1;
